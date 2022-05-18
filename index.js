c58608039ff70e8ce5f05bb4bd002d43a6c54f05
Joon Jung <joonjung55@gmail.com>
2022-05-18 12:36:14 -0400

    commit tree e3343a87d0bd4f3bbb5d2f3e119c2ab97cade411

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const thankyoumessages = [];
+
+function writeCards(names, events) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${events} gift!`); 
+        thankyoumessages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)  
+    }
+    return thankyoumessages;
+}
+
+function countDown() {
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter--)
+    }
+}
